62b00344c51c61fed6a13d2dc99acf5e477d54a1
Adam Kielar <adam@adamkielar.pl>
2020-07-14 15:08:32 +0200

    heroku sendgrid email

--- social_team/social_team/deploy_settings/__init__.py
+++ social_team/social_team/deploy_settings/__init__.py
@@ -7,8 +7,5 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-EMAIL_USE_TLS = True
