8665d3715422088d147058312743f706ead6faeb
Adam Kielar <adam@adamkielar.pl>
2020-07-14 12:32:20 +0200

    heroku settings

--- social_team/social_team/deploy_settings/__init__.py
+++ social_team/social_team/deploy_settings/__init__.py
@@ -3,6 +3,9 @@ import dj_database_url
 
 from social_team.settings import *
 
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+
