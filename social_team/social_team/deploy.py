ed42879afbac573cc2cd085999ff6145550f176e
Adam Kielar <adam@adamkielar.pl>
2020-07-21 10:38:28 +0200

    Removed whitenoise from wsgi

--- social_team/social_team/deploy.py
+++ social_team/social_team/deploy.py
@@ -2,10 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_team.deploy_settings')
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
