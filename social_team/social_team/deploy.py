c088a68ebb2166adeb261a41b32f627dedf505f7
Adam Kielar <adam@adamkielar.pl>
2020-07-21 10:21:51 +0200

    Added S3

--- social_team/social_team/deploy.py
+++ social_team/social_team/deploy.py
@@ -2,6 +2,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_team.deploy_settings')
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
--- social_team/social_team/deploy_settings/__init__.py
+++ social_team/social_team/deploy_settings/__init__.py
@@ -10,3 +10,13 @@ DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
 SENDGRID_SANDBOX_MODE_IN_DEBUG=False
+
+S3_STORAGE_BACKEND = bool(int(os.environ.get('S3_STORAGE_BACKEND', 1)))
+if S3_STORAGE_BACKEND is True:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('S3_STORAGE_BUCKET_REGION', 'us-east-1')
+AWS_QUERYSTRING_AUTH = False
+
